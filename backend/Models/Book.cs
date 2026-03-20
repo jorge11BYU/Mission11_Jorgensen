6d9cccdd3eecf66ba1e236ad31e8f5696cb90af8
Spencer Jorgensen <166751627+jorge11BYU@users.noreply.github.com>
2026-03-20 11:44:36 -0600

    Scaffolded model from database

---
+++ backend/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mission11_Jorgensen.Models;
+
+public partial class Book
+{
+    public int BookId { get; set; }
+
+    public required string Title { get; set; }
+
+    public required string Author { get; set; }
+
+    public required string Publisher { get; set; }
+
+    public required string Isbn { get; set; }
+
+    public required string Classification { get; set; }
+
+    public required string Category { get; set; }
+
+    public int PageCount { get; set; }
+
+    public double Price { get; set; }
+}
